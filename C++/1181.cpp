bff399dad62e02ed0920961af29e296aabbaa366
dayoungss <qbddjehd@naver.com>
2021-05-10 11:44:35 +0900

    1181 and 2164

---
+++ C++/1181.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool compare(string, string);
+
+//백준 1181번 단어 정렬
+
+int main()
+{
+	int n;
+	string tmp;
+	cin >> n;
+	vector <string> word;
+	for (int i = 0; i < n; i++) {
+		cin >> tmp;
+		word.push_back(tmp);
+	}
+	sort(word.begin(), word.end(), compare);
+	tmp.clear();
+	for (int i = 0; i < n; i++) {
+		if (tmp == word[i])continue;
+		cout << word[i] << '\n';
+		tmp = word[i];
+	}
+
+}
+
+bool compare(string a, string b) {
+	if (a.size() == b.size()) {
+		return a < b;
+	}
+	else {
+		return a.size() < b.size();
+	}
+}
---
+++ C++/2164.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <queue>
+using namespace std;
+
+//백준 2164 카드2
+
+int main()
+{
+	int n;
+	queue<int> q;
+	cin >> n;
+
+	for (int i = 1; i <= n; ++i) {
+		q.push(i);
+	}
+
+	while (q.size() > 1) {
+		q.pop();
+		q.push(q.front());
+		q.pop();
+	}
+
+	cout << q.front();
+}
