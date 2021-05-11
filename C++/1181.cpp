4a1db333cb4bcb42a4fad0a8a14974647b2876b3
dayoungss <53688895+dayoungss@users.noreply.github.com>
2021-05-11 11:56:59 +0900

    Update 1181.cpp

--- C++/1181.cpp
+++ C++/1181.cpp
@@ -13,14 +13,15 @@ int main()
 	int n;
 	string tmp;
 	cin >> n;
-	vector <string> word;
-	for (int i = 0; i < n; i++) {
+	vector <string> word; // string type의 vector
+	for (int i = 0; i < n; i++) { // 입력 받고 뒤에 넣어줌
 		cin >> tmp;
 		word.push_back(tmp);
 	}
-	sort(word.begin(), word.end(), compare);
+	sort(word.begin(), word.end(), compare); //길이 순 정렬
+	
 	tmp.clear();
-	for (int i = 0; i < n; i++) {
+	for (int i = 0; i < n; i++) { //알파벳 순 정렬
 		if (tmp == word[i])continue;
 		cout << word[i] << '\n';
 		tmp = word[i];
