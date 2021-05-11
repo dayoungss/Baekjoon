5fb18df68279ccbc3f466db29d8b8694735d6823
dayoungss <53688895+dayoungss@users.noreply.github.com>
2021-05-11 12:01:45 +0900

    Update 2164.cpp

--- C++/2164.cpp
+++ C++/2164.cpp
@@ -7,18 +7,20 @@ using namespace std;
 int main()
 {
 	int n;
-	queue<int> q;
+	queue<int> q; // queue 생성
 	cin >> n;
 
-	for (int i = 1; i <= n; ++i) {
+	for (int i = 1; i <= n; ++i) { // 1부터 n까지 queue에 넣음
 		q.push(i);
 	}
 
-	while (q.size() > 1) {
-		q.pop();
-		q.push(q.front());
-		q.pop();
+	while (q.size() > 1) { //queue의 원소 개수가 1이 될때까지
+		q.pop(); //앞에거 뺌
+		q.push(q.front()); //맨 앞에거 뒤에 넣음
+		q.pop(); //맨 앞에거 뺌
 	}
-
+	// 왜 q.push(q.pop())이 안될까?.. 찾아보니까 pop은 리턴값이 없어서 안된대
+	// 왜 없지? 왜 함수를 그렇게 만들었지? ;;;;
+	// 난 제주도 간다 안녕
 	cout << q.front();
 }
