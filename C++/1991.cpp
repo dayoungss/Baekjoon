801bdcd5ab88a5846664a334620da4459b08c9df
dayoungss <qbddjehd@naver.com>
2021-05-16 16:40:36 +0900

    1191 and mystrcmp

---
+++ C++/1991.cpp
@@ -0,0 +1,81 @@
+﻿#include <iostream>
+using namespace std;
+
+//백준 1991 트리순회
+
+typedef struct _tree {
+	char value;
+	_tree* left;
+	_tree* right;
+}_tree;
+
+_tree tree[30];
+
+void preOrder(_tree *);
+void inOrder(_tree *);
+void postOrder(_tree *);
+
+int main()
+{
+	int n; 
+	char value, left, right;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> value >> left >> right;
+		int index = value - 'A';
+		tree[index].value = value;
+		if (left == '.') {
+			tree[index].left = NULL;
+		}else  tree[index].left = &tree[left - 'A'];
+		if (right == '.') {
+			tree[index].right = NULL;
+		}else  tree[index].right = &tree[right - 'A'];
+	}
+
+	preOrder(&tree[0]);
+	cout << '\n';
+	inOrder(&tree[0]);
+	cout << '\n';
+	postOrder(&tree[0]);
+	cout << '\n';
+}
+
+void preOrder(_tree *root){
+	if (root->value == '.') {
+		return;
+	}
+	cout << root->value;
+	if (root->left != NULL) {
+		preOrder(root->left);
+	}
+	if (root->right != NULL) {
+		preOrder(root->right);
+	}
+}
+
+void inOrder(_tree* root) {
+	if (root->value == '.') {
+		return;
+	}
+	if (root->left != NULL) {
+		inOrder(root->left);
+	}
+	cout << root->value;
+	if (root->right != NULL) {
+		inOrder(root->right);
+	}
+}
+
+void postOrder(_tree* root) {
+	if (root->value == '.') {
+		return;
+	}
+	if (root->left != NULL) {
+		postOrder(root->left);
+	}
+	if (root->right != NULL) {
+		postOrder(root->right);
+	}
+	cout << root->value;
+}
---
+++ C++/mystrcmp.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+//mystrcmp
+
+int mystrcmp (string, string);
+
+int main()
+{
+	string left, right;
+	cout << "first input : ";
+	cin >> left;
+	cout << "second input : ";
+	cin >> right;
+
+	cout << "mystrcmp result is " << mystrcmp(left, right) << endl;
+}
+
+int mystrcmp(const string left, const string right) {
+	if (left.size() != right.size())
+		return left.size() - right.size();
+	else {
+		int i = 0;
+		while (left.size() > i && right.size() > i) {
+			if (left.at(i) != right.at(i))
+				return left.at(i) - right.at(i);
+			i++;
+		}
+		return 0;
+	}
+}
