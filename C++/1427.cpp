d3b8bb213d40026819eae1729072d46ac55190c4
dayoungss <qbddjehd@naver.com>
2021-06-08 21:41:10 +0900

    1427

---
+++ C++/1427.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+//백준 1427 소트인사이드
+int main()
+{
+	string num;
+	cin >> num;
+	sort(num.begin(), num.end(), greater<>());
+	cout << num;
+}
